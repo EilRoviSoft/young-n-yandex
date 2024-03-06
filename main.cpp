312dbe73a2a846300a60d4db1d979b8631f6dae7
NULL the High Priest <el.rovisoft@gmail.com>
2024-03-06 23:51:03 +0300

    OK

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <cstdint>
 
-namespace util {
-    template <class _T>
-    _T scan(std::istream& is = std::cin) {
-        _T temp;
-        is >> temp;
-        return temp;
-    }
+int main() {
+	//sil - spaces_in_line
+	int64_t total = 0, n, sil;
+
+	std::cin >> n;
+
+	for (uint64_t i = 0; i != n; i++) {
+		std::cin >> sil;
+		total += sil / 4;
+		sil %= 4;
+
+		if (sil == 3)
+			total += 2;
+		else if (sil <= 2)
+			total += sil;
+	}
+
+	std::cout << total;
 }
 
-int main() {
-    int a = util::scan <int>();
-}
+//5 1 4 12 9 0
+//8
