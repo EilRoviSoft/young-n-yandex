87466f3424e18d5df6bb9c4eb0c67f4f9af10143
NULL the High Priest <el.rovisoft@gmail.com>
2024-03-06 22:33:50 +0300

    Util

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <cstdint>
 
+namespace util {
+    template <class _T>
+    _T read(std::istream& is = std::cin) {
+        _T temp;
+        is >> temp;
+        return temp;
+    }
+}
+
 int main() {
     
 }
