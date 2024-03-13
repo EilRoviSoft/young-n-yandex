72b1b0f26e29aba921b3aca588772505555cbf03
NULL the High Priest <el.rovisoft@gmail.com>
2024-03-13 19:06:27 +0300

    smth

--- main.cpp
+++ main.cpp
@@ -1,15 +1,6 @@
 #include <iostream>
 #include <cstdint>
 
-namespace util {
-	template <class _T>
-	_T scan(std::istream& is = std::cin) {
-		_T temp;
-		is >> temp;
-		return temp;
-	}
-}
-
 int main() {
-	int a = util::scan <int>();
+	return 0;
 }
