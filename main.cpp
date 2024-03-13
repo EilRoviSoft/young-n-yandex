b638c35f25a43a46899fab1d37346b0227e545fa
NULL the High Priest <el.rovisoft@gmail.com>
2024-03-13 19:06:04 +0300

    NOT OK SADLY

--- main.cpp
+++ main.cpp
@@ -1,119 +1,67 @@
-#include <array>
-#include <cstdint>
+#include <cmath>
+#include <functional>
+#include <iomanip>
 #include <iostream>
-#include <set>
-#include <string>
-#include <vector>
+#include <tuple>
 
-enum Month {
-	Dec = 11, Jan = 0, Feb,
-	Mar, Apr, May,
-	Jun, Jul, Aug,
-	Sep, Oct, Nov
-};
+std::tuple <bool, double> calc(double l, double x1, double v1, double x2, double v2) {
+	if (x1 == x2 || l - x1 == x2 || l - x2 == x1)
+		return { true, 0.f };
+	if (v1 == 0 && v2 == 0)
+		return { false, 0.f };
 
-std::array <const std::string, 12> months_names = {
-		"January", "February", "March",
-		"April", "May", "June",
-		"July", "August", "September",
-		"October", "November", "December"
-};
+	double ax1 = std::abs(x1),
+		av1 = std::abs(v1),
+		ax2 = std::abs(x2),
+		av2 = std::abs(v2),
+		dx = std::abs(x1 - x2),
+		dv = std::abs(v1 - v2);
 
-Month get_month_id(std::string month) {
-	for (size_t i = 0; i != 12; i++)
-		if (month == months_names[i]) {
-			return (Month)i;
-		}
-}
+	if (v1 == v2) {
+		double x = (x1 + x2) / 2, dv = v1;
+		if (x < l / 2 && dv < 0)
+			dx = x;
+		else if (x > l / 2 && dv < 0)
+			dx = l / 2 + x;
+		else if (x < l / 2 && dv > 0)
+			dx = l / 2 - x;
+		else if (x > l / 2 && dv > 0)
+			dx = l - x;
+		return { true, std::abs(dx / dv) };
+	} else if (av1 == av2 && (v1 > 0 && v2 < 0 || v1 < 0 && v2 > 0))
+		return { true, (ax1 + ax2) / (av1 + av2) };
 
-enum Week {
-	Mon = 0, Tue, Wed, Thu, Fri, Sat, Sun
-};
-
-std::array <const std::string, 7> days_of_a_week_names = {
-	"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
-};
-
-Week get_day_of_a_week_id(std::string day_of_a_week) {
-	for (size_t i = 0; i != 7; i++)
-		if (day_of_a_week == days_of_a_week_names[i]) {
-			return (Week)i;
-		}
-}
-
-struct date_t {
-	Month m;
-	uint16_t d;
-};
-
-bool operator<(date_t lhs, date_t rhs) {
-	if (lhs.m == rhs.m)
-		return lhs.d < rhs.d;
-	return lhs.m < rhs.m;
-}
-
-//std::array <size_t, 12> months_offset;
-
-std::vector <date_t> alloc_days_of_a_year(size_t year) {
-	bool is_leap_year = (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
-	size_t filled = 0;
-	std::array <size_t, 12> months = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	std::vector <date_t> result;
-
-	if (is_leap_year) {
-		months[Feb]++;
-		result.resize(366);
-	} else result.resize(365);
-
-	/*months_offset[0] = 0;
-	for (size_t i = 1; i != 12; i++)
-		months_offset[i] = months[i] + months_offset[i - 1];*/
-
-	for (size_t month_id = 0; month_id != 12; month_id++) {
-		for (size_t i = 0; i != months[month_id]; i++) {
-			result[filled] = { (Month)month_id, (uint16_t)(i + 1) };
-			filled++;
-		}
+	if (v1 * v2 > 0) {
+		if (dx < l)
+			return { true, dx / dv };
+		else
+			return { true, (dx + l) / dv };
+	} else if (v1 * v2 < 0) {
+		if (dx < l)
+			return { true, dx / dv };
+		else
+			return { true, std::abs(dx - l) / dv };
+	} else if (v1 == 0 && v2 > 0) {
+		if (x1 < x2) 
+			dx = l - dx;
+		return { true, dx / dv };
+	} else if (v1 > 0 && v2 == 0) {
+		if (x1 > x2)
+			dx = l - dx;
+		return { true, dx / dv };
 	}
-
-	return result;
 }
 
 int main() {
-	size_t amount_of_holidays, current_year, min = 0, max = 0;
-	Week starting_day;
-	std::array <size_t, 7> days_of_a_week;
-	std::set <date_t> holidays;
-	std::vector <date_t> days_of_a_year;
+	int64_t length, x1, v1, x2, v2;
 
-	std::cin >> amount_of_holidays >> current_year;
-	for (size_t i = 0; i != amount_of_holidays; i++) {
-		std::string month;
-		uint16_t day;
-		std::cin >> day >> month;
-		holidays.emplace(date_t{ get_month_id(month), day });
-	}
-	{
-		std::string day_of_a_week;
-		std::cin >> day_of_a_week;
-		starting_day = get_day_of_a_week_id(day_of_a_week);
-	}
-	days_of_a_year = alloc_days_of_a_year(current_year);
-	for (auto& it : days_of_a_week)
-		it = 0;
+	std::cin >> length >> x1 >> v1 >> x2 >> v2;
+	std::cout << std::fixed << std::setprecision(10);
 
-	for (size_t i = 0; i != days_of_a_year.size(); i++) {
-		if (!holidays.contains(days_of_a_year[i]))
-			days_of_a_week[(i + starting_day) % 7]++;
-	}
+	auto [result, time] = calc(length, x1, v1, x2, v2);
 
-	min = max = 0;
-	for (size_t i = 1; i != 7; i++) {
-		if (days_of_a_week[min] > days_of_a_week[i])
-			min = i;
-		if (days_of_a_week[max] < days_of_a_week[i])
-			max = i;
-	}
-
-	std::cout << days_of_a_week_names[max] << ' ' << days_of_a_week_names[min];
+	if (result)
+		std::cout << "YES\n" << time;
+	else
+		std::cout << "NO";
 }
