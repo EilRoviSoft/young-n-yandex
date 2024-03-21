eee6a9dd027b2c81196793625909fd26892b491a
NULL the High Priest <el.rovisoft@gmail.com>
2024-03-21 16:58:43 +0300

    OK

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
-#include <iostream>
 #include <cstdint>
+#include <iostream>
+#include <string>
 
 int main() {
+	bool has_lower = false, has_upper = false, has_number = false;
+	std::string input;
+
+	std::cin >> input;
+
+	if (input.size() < 8) {
+		std::cout << "NO";
+		return 0;
+	}
+
+	for (auto it : input) {
+		if (it >= 'a' && it <= 'z')
+			has_lower = true;
+		else if (it >= 'A' && it <= 'Z')
+			has_upper = true;
+		else if (it >= '0' && it <= '9')
+			has_number = true;
+	}
+
+	std::cout << (has_lower && has_upper && has_number ? "YES" : "NO");
+
 	return 0;
 }
